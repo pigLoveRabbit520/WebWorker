2ecc61b9304a71130ff3e1f5b28df7f3fcebafa3
salamander <1906747819@qq.com>
2018-08-20 18:27:09 +0800

    fix bug

--- src/Slim/RouteGroup.php
+++ src/Slim/RouteGroup.php
@@ -33,9 +33,9 @@ class RouteGroup extends Routable implements RouteGroupInterface
     /**
      * Invoke the group to register any Routable objects within it.
      *
-     * @param App $app The App instance to bind/pass to the group callable
+     * @param $app The App instance to bind/pass to the group callable
      */
-    public function __invoke(App $app = null)
+    public function __invoke($app = null)
     {
         $callable = $this->resolveCallable($this->callable);
         if ($callable instanceof Closure && $app !== null) {
